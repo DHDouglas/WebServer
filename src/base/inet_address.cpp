13923989b68c07c415ad040802acc160dcdda192
douglas <dhdocean@gmail.com>
2025-03-06 18:25:22 +0800

    Add InetAddress class

---
+++ src/base/inet_address.cpp
@@ -0,0 +1,115 @@
+#include "inet_address.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+using namespace std;
+
+static_assert(sizeof(InetAddress) == sizeof(struct sockaddr_in6),
+              "InetAddress is same size as sockaddr_in6");
+static_assert(offsetof(sockaddr_in, sin_family) == 0, "sin_family offset 0");
+static_assert(offsetof(sockaddr_in6, sin6_family) == 0, "sin6_family offset 0");
+static_assert(offsetof(sockaddr_in, sin_port) == 2, "sin_port offset 2");
+static_assert(offsetof(sockaddr_in6, sin6_port) == 2, "sin6_port offset 2");
+
+InetAddress::InetAddress(uint16_t port, bool loop_back_only, bool ipv6) {
+    if (ipv6) {
+        memset(&addr6_, 0, sizeof(addr6_)); 
+        addr6_.sin6_family = AF_INET6; 
+        in6_addr ip = loop_back_only ? in6addr_loopback : in6addr_any; 
+        addr6_.sin6_addr = ip;
+        addr6_.sin6_port = htons(port); 
+    } else {
+        memset(&addr_, 0, sizeof(addr_));
+        addr_.sin_family = AF_INET; 
+        in_addr_t ip = loop_back_only ? INADDR_LOOPBACK : INADDR_ANY; 
+        addr_.sin_addr.s_addr = htonl(ip); 
+        addr_.sin_port = htons(port); 
+    }
+}
+
+InetAddress::InetAddress(string ip, uint16_t port, bool ipv6) {
+    if (ipv6 || strchr(ip.c_str(), ':')) {
+        memset(&addr6_, 0, sizeof(addr6_)); 
+        addr6_.sin6_family = AF_INET6;
+        addr6_.sin6_port = htons(port); 
+        if (::inet_pton(AF_INET6, ip.c_str(), &addr6_.sin6_addr) <= 0) {
+            LOG_SYSERR << "InetAddress::InetAddress inet_pton"; 
+        }
+    } else {
+        memset(&addr_, 0, sizeof(addr_)); 
+        addr_.sin_family = AF_INET;
+        addr_.sin_port = htons(port); 
+        if (::inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr) <= 0) {
+            LOG_SYSERR << "InetAddress::InetAddress inet_pton"; 
+        }
+    }
+}
+
+string InetAddress::getIpPortString() const {
+    return getIpString() + ":" + to_string(getPort()); 
+}
+
+string InetAddress::getIpString() const {
+    char buf[64] = ""; 
+    if (addr_.sin_family == AF_INET) {
+        ::inet_ntop(AF_INET, &addr_.sin_addr, buf, static_cast<socklen_t>(sizeof(buf)));
+    } else {
+        ::inet_ntop(AF_INET6, &addr6_.sin6_addr, buf, static_cast<socklen_t>(sizeof(buf)));
+    }
+    return buf; 
+}
+
+uint16_t InetAddress::getPort() const {
+    return ntohs(addr_.sin_port);  // sockaddr_in与sockaddr_in6中sin_port偏移相同. 
+}
+
+const struct sockaddr* InetAddress::getSockAddr() const {
+    return sockAddrCast(&addr_);; 
+}
+
+socklen_t InetAddress::getAddrLen() const {
+    if (addr_.sin_family == AF_INET) {
+        return sizeof(addr_); 
+    } else {
+        return sizeof(addr6_);
+    }
+}
+
+
+struct sockaddr* InetAddress::sockAddrCast(struct sockaddr_in* addr) {
+    return static_cast<struct sockaddr*>(static_cast<void*>(addr));
+}
+
+struct sockaddr* InetAddress::sockAddrCast(struct sockaddr_in6* addr) {
+    return static_cast<struct sockaddr*>(static_cast<void*>(addr));
+}
+
+const struct sockaddr* InetAddress::sockAddrCast(const struct sockaddr_in* addr) {
+    return static_cast<const struct sockaddr*>(static_cast<const void*>(addr));
+}
+
+const struct sockaddr* InetAddress::sockAddrCast(const struct sockaddr_in6* addr) {
+    return static_cast<const struct sockaddr*>(static_cast<const void*>(addr));
+}
+
+
+struct sockaddr_in6 InetAddress::getLocalAddrBySockfd(int sockfd) {
+    struct sockaddr_in6 addr6_;
+    memset(&addr6_, 0, sizeof(addr6_)); 
+    socklen_t addrlen = static_cast<socklen_t>(sizeof(addr6_));
+    if (::getsockname(sockfd, InetAddress::sockAddrCast(&addr6_), &addrlen) < 0) {
+        LOG_SYSERR << "InetAddress::getLocalAddrBySockfd";
+    }
+    return addr6_;
+}
+
+struct sockaddr_in6 InetAddress::getPeerAddrBySockfd(int sockfd) {
+    struct sockaddr_in6 addr6_;
+    memset(&addr6_, 0, sizeof(addr6_)); 
+    socklen_t addrlen = static_cast<socklen_t>(sizeof(addr6_));
+    if (::getpeername(sockfd, InetAddress::sockAddrCast(&addr6_), &addrlen) < 0) {
+      LOG_SYSERR << "InetAddress::getPeerAddrBySockfd";
+    }
+    return addr6_;
+}
---
+++ src/base/inet_address.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <sys/socket.h>
+
+#include "logger.h"
+
+class InetAddress {
+public:
+    InetAddress(std::string ip, uint16_t port, bool ipv6 = false); 
+    explicit InetAddress(uint16_t port = 0, bool loopbackOnly = false, bool ipv6 = false);
+    explicit InetAddress(const struct sockaddr_in& addr) : addr_(addr) {}
+    explicit InetAddress(const struct sockaddr_in6& addr) : addr6_(addr) {}
+
+    void setSockAddrInet6(const struct sockaddr_in6& addr) { addr6_ = addr; }
+
+    sa_family_t getFamily() const { return addr_.sin_family; }
+    std::string getIpPortString() const;
+    std::string getIpString() const;
+    uint16_t getPort() const;
+    socklen_t getAddrLen() const; 
+    uint32_t ipv4NetEndian() const { return addr_.sin_addr.s_addr; }
+    uint16_t portNetEndian() const { return addr_.sin_port; }
+    const struct sockaddr* getSockAddr() const;
+
+    static struct sockaddr* sockAddrCast(struct sockaddr_in* addr);
+    static struct sockaddr* sockAddrCast(struct sockaddr_in6* addr);
+    static const struct sockaddr* sockAddrCast(const struct sockaddr_in* addr);
+    static const struct sockaddr* sockAddrCast(const struct sockaddr_in6* addr);
+
+    static struct sockaddr_in6 getLocalAddrBySockfd(int sockfd); 
+    static struct sockaddr_in6 getPeerAddrBySockfd(int sockfd); 
+
+private:
+    union {
+        struct sockaddr_in addr_; 
+        struct sockaddr_in6 addr6_; 
+    }; 
+};
